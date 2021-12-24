fbbfa14fc350892eb96feb76a7620a0150065343
tomasz.pionka <reports.poland@kinesso.com>
2021-12-24 23:43:00 +0100

    Added test flow for cadreon

--- src/connectors/adform/auth.py
+++ src/connectors/adform/auth.py
@@ -1,10 +1,10 @@
 import requests
+
 from src.util.credentials import read_file_key, update_file
 from src.util.helpers import wait_until, branch_operator
 
-client_storage_filepath = ""
-auth_storage_filepath = ""
-token_dict_path = []
+auth_filepath = "auth.json"
+auth_dict_path = ['accounts', 'cadreon']
 
 
 def read_token(filepath: str, dict_path: list):
@@ -20,45 +20,46 @@ def create_token(client_id: str, client_secret: str):
     dict_path = "access_token"
     token_url = "https://id.adform.com/sts/connect/token"
     token_data = {
-            "grant_type": "client_credentials",
-            "client_id": client_id,
-            "client_secret": client_secret,
-            "scope": "https://api.adform.com/scope/buyer.stats",
-        }
+        "grant_type": "client_credentials",
+        "client_id": client_id,
+        "client_secret": client_secret,
+        "scope": "https://api.adform.com/scope/buyer.stats",
+    }
 
     response = requests.post(
         url=token_url,
         data=token_data,
     )
-
+    print("create_token", response.status_code)
     return response.json()[dict_path]
 
 
 def update_token(filepath: str, dict_path: list, access_token: object):
-    update_file(filepath, access_token, dict_path)
-    return
+    return update_file(filepath, access_token, dict_path)
 
 
-def post_operation(filepath: str, dict_path: list, client_id: str, client_secret: str, scope: list):
+def post_operation(filepath: str, dict_path: list, client_id: str, client_secret: str, body: dict):
     access_token = read_token(
         filepath=filepath,
         dict_path=dict_path)
-    body = {
-        "dimensions": None,
-        "metrics": None,
-        "filter": None,
-    }
 
-    base_url = ""
-    headers = {}
-    json = {}
+    base_url = "https://api.adform.com/v1/buyer/stats/data"
+    headers = {
+        "Content-Type": "application/json",
+        "Authorization": "Bearer {}".format(access_token)
+    }
+    json = {
+        'dimensions': body['dimensions'],
+        'metrics': body['metrics'],
+        'filter': body['filter']
+    }
 
     response = requests.post(
         base_url,
         headers=headers,
         json=json
     )
-
+    print("post_operation", response.status_code)
     if response.status_code != 202:
         wait_until(10)
         access_token = create_token(
@@ -70,41 +71,43 @@ def post_operation(filepath: str, dict_path: list, client_id: str, client_secret
             dict_path=dict_path,
             access_token=access_token
         )
-        post_operation(
+        return post_operation(
             filepath=filepath,
             dict_path=dict_path,
             client_id=client_id,
-            client_secret=client_secret
+            client_secret=client_secret,
+            body=body
         )
     else:
         location = response.headers['Location']
         operation = response.headers['Operation-Location']
         while not branch_operator(
-            input_function=read_operation_status(
-                filepath=filepath,
-                scope=scope,
-                operation=operation
-            ),
-            condition=200
+                input_function=read_operation_status(
+                    filepath=filepath,
+                    dict_path=dict_path,
+                    operation=operation
+                ),
+                condition=200
         ):
             wait_until(20)
         # TODO invoke function uploading data after reading location
         while not branch_operator(
-            input_function=read_location(
-                filepath=filepath,
-                scope=scope,
-                location=location
-            ),
-            condition=200
+                input_function=read_location(
+                    filepath=filepath,
+                    dict_path=dict_path,
+                    location=location
+                ),
+                condition=200
         ):
             wait_until(20)
-    return
+
+        return read_location_data(filepath, dict_path, location)
 
 
-def read_operation_status(filepath, scope, operation):
+def read_operation_status(filepath, dict_path, operation):
     access_token = read_token(
         filepath=filepath,
-        dict_path=scope
+        dict_path=dict_path
     )
 
     base_url = "https://api.adform.com/{}".format(operation)
@@ -114,14 +117,14 @@ def read_operation_status(filepath, scope, operation):
         url=base_url,
         headers=headers
     )
-
+    print("read_operation_status", response.status_code)
     return response.status_code
 
 
-def read_location(filepath: str, scope: list, location: str):
+def read_location(filepath: str, dict_path: list, location: str):
     access_token = read_token(
         filepath=filepath,
-        dict_path=scope
+        dict_path=dict_path
     )
 
     base_url = "https://api.adform.com/{}".format(location)
@@ -131,14 +134,14 @@ def read_location(filepath: str, scope: list, location: str):
         url=base_url,
         headers=headers
     )
+    print("read_location", response.status_code)
+    return response.status_code
 
-    return response.status_code != 200
 
-
-def read_location_data(filepath: str, scope: list, location: str) -> dict:
+def read_location_data(filepath: str, dict_path: list, location: str) -> dict:
     access_token = read_token(
         filepath=filepath,
-        dict_path=scope
+        dict_path=dict_path
     )
     base_url = "https://api.adform.com/{}".format(location)
     headers = {"Authorization": "Bearer {}".format(access_token)}
@@ -147,22 +150,45 @@ def read_location_data(filepath: str, scope: list, location: str) -> dict:
         url=base_url,
         headers=headers
     )
-
+    print("read_locaiton_data", response.status_code)
     return response.json()
 
 
-def main(filepath: str, scope: str) -> object:
+def main(filepath: str, dict_path: list) -> object:
     client_id = read_file_key(
         filepath=filepath,
-        path=[scope, "client_id"]
+        path=['accounts', 'cadreon', 'client_id']
     )
     client_secret = read_file_key(
         filepath=filepath,
-        path=[scope, "client_secret"]
+        path=['accounts', 'cadreon', 'client_secret']
     )
-    # TODO get report (client_id, client_secret, scope)
-    return
+    body = dict()
+    body['dimensions'] = ["date", "client", "campaign", "page"]
+    body['metrics'] = [
+        {
+            "metric": "impressions",
+            "specs": {"adUniqueness": "campaignUnique"}
+        },
+        {
+            "metric": "clicks",
+            "specs": {"adUniqueness": "campaignUnique"}
+        },
+        {
+            "metric": "sysvarProductCount"
+        }
+    ]
+    body['filter'] = {
+        "date": "lastWeek"
+    }
+    return post_operation(
+        filepath,
+        ['accounts', 'cadreon', 'token'],
+        client_id,
+        client_secret,
+        body
+    )
 
 
 if __name__ == "__main__":
-    pass
+    print(main(auth_filepath, auth_dict_path))
