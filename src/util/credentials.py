62dd7216b2170165802e3a11df5e0198b9eadc4e
tomasz.pionka <reports.poland@kinesso.com>
2021-12-26 10:20:26 +0100

    Moved functions

--- src/util/credentials.py
+++ src/util/credentials.py
@@ -1,27 +1,12 @@
 import json
-from src.util.helpers import recursive_get
+from src.util.helpers import recursive_get, read_file, write_file
 
 
-def read_file(filepath: object):
-    try:
-        with open(filepath, "r") as f:
-            data = json.load(f)
-    except Exception as e:
-        print(e)
-        data = filepath
-    return data
-
-
-def read_file_key(filepath: object, path: list):
+def read_file_key(filepath: str, path: list):
     data = read_file(filepath)
     target_key = path[-1]
     sub_dict = recursive_get(data, *path[:-1])
     return sub_dict[target_key]
-
-
-def write_file(filepath: str, data: object):
-    with open(filepath, "w") as f:
-        json.dump(data, f, indent=2)
 
 
 def update_file(filepath: str, new_value: object, path: list):
--- src/util/helpers.py
+++ src/util/helpers.py
@@ -1,6 +1,21 @@
 from functools import reduce
 from time import sleep
+import json
 
+
+def read_file(filepath: str):
+    try:
+        with open(filepath, "r") as f:
+            data = json.load(f)
+    except Exception as e:
+        print(e)
+        data = filepath
+    return data
+
+
+def write_file(filepath: str, data: object):
+    with open(filepath, "w") as f:
+        json.dump(data, f, indent=2)
 
 def recursive_get(d, *keys):
     return reduce(lambda c, k: c.get(k, {}), keys, d)
